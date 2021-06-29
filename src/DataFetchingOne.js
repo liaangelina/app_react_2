85fe714eff70a30b39986c079455f1ab0e5bd19f
liaangelina <62002438+liaangelina@users.noreply.github.com>
2021-06-29 15:16:04 +0700

    Create DataFetchingOne.js

---
+++ src/DataFetchingOne.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function DataFetchingOne() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        axios
+            .get('https://dev.farizdotid.com/api/daerahindonesia/provinsi')
+            .then(res => {
+                console.log(res)
+                setPosts(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
+
+    return (
+        <div>
+            {posts.map(post => (
+                <option value={post.id}>{post.nama}</option>
+            ))}
+        </div>
+    )
+}
+
+export default DataFetchingOne
