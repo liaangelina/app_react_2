4f02c1cc20a23e2f59a060828c3f40a4b3bf8cc5
liaangelina <62002438+liaangelina@users.noreply.github.com>
2021-06-29 15:17:01 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import DataFetchingOne from './DataFetchingOne'
+
+function App() {
+  return (
+    <div className='App'>
+      <DataFetchingOne />
+    </div>
+  )
+}
+
+export default App
