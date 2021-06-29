8c25feda1218b1648a03a430ff5e53260562cc73
liaangelina <62002438+liaangelina@users.noreply.github.com>
2021-06-29 15:27:46 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from 'react'
-import './App.css'
-import DataFetchingOne from './DataFetchingOne'
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, {useState} from "react"
+import "./App.css"
+import DataFetchingOne from "./DataFetchingOne"
 
 function App() {
+  const [foodState, setFoodState] = useState("...");
+
   return (
-    <div className='App'>
-      <DataFetchingOne />
+    <div className="container p-5">
+      <select className="custom-select" value={foodState} onChange={(e) => {
+        const selectedFood = e.target.value;
+        setFoodState(selectedFood);
+      }}>
+        <DataFetchingOne />
+      </select>
+      {foodState}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
